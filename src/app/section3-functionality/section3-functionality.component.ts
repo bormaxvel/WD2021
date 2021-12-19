615c13dc58bc258fa9600c4e7c0a7e1949ad6779
Borysman <bormaxv@gmail.com>
2021-12-19 23:36:03 +0200

    апар

--- src/app/section3-functionality/section3-functionality.component.ts
+++ src/app/section3-functionality/section3-functionality.component.ts
@@ -20,6 +20,15 @@ export class Section3FunctionalityComponent implements OnInit {
     this.inputName$ = (<HTMLInputElement>document.getElementById("inputName")).value;
     this.inputEmail$ = (<HTMLInputElement>document.getElementById("inputEmail")).value;
     this.inputText$ = (<HTMLInputElement>document.getElementById("inputText")).value;
+    if (this.inputName$ == ""){
+      this.inputName$ = "'Not Imputted'";
+    }
+    if (this.inputEmail$ == ""){
+      this.inputEmail$ = "'Not Imputted'";
+    }
+    if (this.inputText$ == ""){
+      this.inputText$ = "'Not Imputted'";
+    }
     this.changeVisibility();
   }
 
