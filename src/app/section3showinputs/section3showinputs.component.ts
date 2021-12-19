5c4e81d7253abf60178bf5b5ba58426ccf45902f
Borysman <bormaxv@gmail.com>
2021-12-19 23:27:12 +0200

    dafsgd

--- src/app/section3showinputs/section3showinputs.component.ts
+++ src/app/section3showinputs/section3showinputs.component.ts
@@ -11,7 +11,15 @@ export class Section3showinputsComponent implements OnInit {
   constructor(private service:MainLogicService) { }
   inputstoinsert:string[] = this.service.inputs
   ngOnInit(): void {
-    console.log(this.service.inputs);
+    if (this.inputstoinsert[0] == ""){
+      this.inputstoinsert[0] = "'Not Imputted'";
+    }
+    if (this.inputstoinsert[1] == ""){
+      this.inputstoinsert[1] = "'Not Imputted'";
+    }
+    if (this.inputstoinsert[2] == ""){
+      this.inputstoinsert[2] = "'Not Imputted'";
+    }
   }
 
 }
